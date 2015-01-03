c938aa32f72aa4e99747249e85fc66d2024b4445
Thejan Rajapakshe <coder.clix@gmail.com>
2015-01-03 17:36:26 +0530

    Added What is Autism Controller + language

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -18,14 +18,29 @@ class Home extends CI_Controller {
     public function index() {
         $this->en();
     }
-    
+
     public function en() {
+        $this->set_langugage('en');
         $view_data = array();
-        $this->load->view('home',$view_data);
+        $this->load->view('home', $view_data);
     }
-    
+
     public function si() {
+        $this->set_langugage('si');
         $view_data = array();
-        $this->load->view('home',$view_data);
-    } 
+        $this->load->view('home', $view_data);
+    }
+
+    function set_langugage($language = "en") {
+        $this->load->helper('cookie');
+        $cookie = array(
+            'name' => 'lang',
+            'value' => $language,
+            'expire'=> 3600,
+            'domain'=> 'localhost',
+            'path'=>'/'
+        );
+        $this->input->set_cookie($cookie);
+    }
+
 }
