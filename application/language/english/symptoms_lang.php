65d8a21e936f278c48eef4edbe0fad2f5b2efcff
Thejan Rajapakshe <coder.clix@gmail.com>
2015-01-05 14:14:45 +0530

    Syptoms

--- application/language/english/symptoms_lang.php
+++ application/language/english/symptoms_lang.php
@@ -1,9 +1,53 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$lang['symptoms_title_1'] = "Symptoms";
+$lang['symptoms_title_2'] = "1.Difficulty with social communication";
+$lang['symptoms_title_3'] = "2.Difficulty with social interaction";
+$lang['symptoms_title_4'] = "3.Difficulty with social imagination";
+$lang['symptoms_title_5'] = "4.Sensory sensitivity";
+$lang['symptoms_title_6'] = "5.Repetitive actions";
 
-$lang['symptoms_text_1'] = "This is Symptoms Page";
+$lang['symptoms_text_1'] = "People with autism have difficulties with both verbal and nonverbal language. Many have a very literal understanding of language, and think people always mean exactly what they say. They can find it difficult to use or understand:";
+$lang['symptoms_text_2'] = "People with autism often have difficulty recognising or understanding other people's emotions and feelings, and expressing their own, which can make it more difficult for them to fit in socially. They may";
+$lang['symptoms_text_3'] = "Difficulties with social interaction can mean that people with autism find it hard to form friendships: some may want to interact with other people and make friends, but may be unsure how to go about this.";
+$lang['symptoms_text_4'] = "Social imagination allows us to understand and predict other people's behaviour, make sense of abstract ideas, and to imagine situations outside our immediate daily routine. Difficulties with social imagination mean that people with autism, find it hard to:";
+$lang['symptoms_text_5'] = "People with autism may experience some form of sensory sensitivity which is either intensified (hypersensitive) or under-sensitive (hypo-sensitive). This can occur in one or more of the five senses - sight, sound, smell, touch and taste.";
+$lang['symptoms_text_6'] = "Add an introduction paragraph here.";
+
+$lang['symptoms_ul_1_li_1'] = "Not responding to their names when someone calls.";
+$lang['symptoms_ul_1_li_2'] = "Facial expressions or tone of voice.";
+$lang['symptoms_ul_1_li_3'] = "Jokes and sarcasm";
+$lang['symptoms_ul_1_li_4'] = "Common phrases and sayings; an example might be the phrase 'It's cool', which people often say when they think that something is good, but strictly speaking, means that it's a bit cold.";
+$lang['symptoms_ul_1_li_5'] = "Repeating words or phrases over and over without communicative intent.";
+$lang['symptoms_ul_1_li_6'] = "Difficulty communicating needs or desires.";
+$lang['symptoms_ul_1_li_7'] = "Doesn’t understand simple statements or questions.";
+$lang['symptoms_ul_1_li_8'] = "Some people with autism may not speak, or have fairly limited speech. They will usually understand what other people say to them, but prefer to use alternative means of communication themselves, such as sign language or visual symbols.";
+$lang['symptoms_ul_1_li_9'] = "Others will have good language skills, but they may still find it hard to understand the give-and-take nature of conversations, perhaps repeating what the other person has just said (this is known as echolalia) or talking at length about their own interests.";
+$lang['symptoms_ul_1_li_10'] = "It helps if other people speak in a clear, consistent way and give people with autism time to process what has been said to them.";
+$lang['symptoms_ul_1_li_11'] = "Avoids eye contact with the person who is actively communicating with them.";
+
+$lang['symptoms_ul_2_li_1'] = "Not understand the unwritten social rules which most of us pick up without thinking: they may stand too close to another person for example, or start an inappropriate subject of conversation.";
+$lang['symptoms_ul_2_li_2'] = "Not seek comfort from other people and not actively join other children to play.";
+$lang['symptoms_ul_2_li_3'] = "Unusual or inappropriate body language, gestures, and facial expressions (e.g. using facial expressions that don’t match what he or she is saying).";
+$lang['symptoms_ul_2_li_4'] = "Appear to be insensitive because they have not recognised how someone else is feeling.";
+$lang['symptoms_ul_2_li_5'] = "Prefer to spend time alone rather than seeking out the company of other people.";
+$lang['symptoms_ul_2_li_6'] = "Appear to behave 'strangely' or inappropriately, as it is not always easy for them to express feelings, emotions or needs.";
+
+$lang['symptoms_ul_3_li_1'] = "Understand and interpret other people's thoughts, feelings and actions.";
+$lang['symptoms_ul_3_li_2'] = "Predict what will happen next, or what could happen next.";
+$lang['symptoms_ul_3_li_3'] = "Understand the concept of danger, for example that running on to a busy road poses a threat to them.";
+$lang['symptoms_ul_3_li_4'] = "Engage in imaginative play and activities: children with autism may enjoy some imaginative play but prefer to act out the same scenes each time.";
+$lang['symptoms_ul_3_li_5'] = "Love of routines-This routine can extend to always wanting to travel the same way to and from school or work, or eat exactly the same food for breakfast.";
+$lang['symptoms_ul_3_li_6'] = "Cope in new or unfamiliar situations.";
+
+$lang['symptoms_ul_4_li_1'] = "A person with autism may find certain background sounds, which other people ignore or block out, unbearably loud or distracting. This can cause anxiety or even physical pain.";
+$lang['symptoms_ul_4_li_2'] = "People who are hypo-sensitive may not feel pain or extremes of temperature.";
+$lang['symptoms_ul_4_li_3'] = "Some may rock, spin or flap their hands to stimulate sensation, to help with balance and posture or to deal with stress.";
+$lang['symptoms_ul_4_li_4'] = "People with sensory sensitivity may also find it harder to use their body awareness system. This system tells us where our bodies are, so for those with reduced body awareness, it can be harder to navigate rooms avoiding obstructions, stand at an appropriate distance from other people and carry out 'fine motor' tasks such as tying shoelaces.";
+$lang['symptoms_ul_4_li_5'] = "A number of children with autism do not like cuddling or being touched like other children do. It is wrong to say that all children with autism are like that. Many will hug a relative - usually the mother, father, grandmother, grandfather, teacher, and or sibling(s) - and enjoy it greatly.";
+
+$lang['symptoms_ul_5_li_1'] = "Repetitive body movements (hand flapping, rocking, spinning); moving constantly.";
+$lang['symptoms_ul_5_li_2'] = "Fascinated by spinning objects, moving pieces, or parts of toys (e.g. spinning the wheels on a race car, instead of playing with the whole car).";
+$lang['symptoms_ul_5_li_3'] = "Obsessive attachment to unusual objects (rubber bands, keys, light switches).";
+
+?>
--- application/views/symptoms.php
+++ application/views/symptoms.php
@@ -10,11 +10,247 @@ $this->load->view('partial/header');
     </div>
     <div class="row clearfix">
         <div class="col-md-12 column">
-            <p> 
-                <?php echo $text_1; ?>
-            </p>
+            <h3>
+                <?php echo $title_1; ?>
+            </h3>
         </div>
     </div>
+    <div class="row clearfix">
+        <div class="col-md-10 column">
+            <h4>
+                <?php echo $title_2; ?>
+            </h4>
+        </div>
+    </div>
+    <div class="row clearfix">
+        <div class="col-md-10 column col-md-offset-1">
+            <p class="text-justify">
+                <?php echo $text_1; ?>
+            </p>
+            <div class="row clearfix">
+                <div class="col-md-5 column  col-md-offset-1">
+                    <ul>
+                        <li class="text-justify"><?php echo $ul_1_li_1; ?></li>
+                        <li class="text-justify"><?php echo $ul_1_li_2; ?></li>
+                        <li class="text-justify"><?php echo $ul_1_li_3; ?></li>
+                        <li class="text-justify"><?php echo $ul_1_li_4; ?></li>
+                        <li class="text-justify"><?php echo $ul_1_li_5; ?></li>
+                        <li class="text-justify"><?php echo $ul_1_li_6; ?></li>
+                        <li class="text-justify"><?php echo $ul_1_li_7; ?></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 column">
+                    <img alt="" width="230px" class="img-rounded" src="<?php echo base_url('assests/images/info-portal/symptoms_1.jpg'); ?>" />
+                </div>
+            </div>
+            <div class="row clearfix">
+                <div class="col-md-5 column  col-md-offset-1">
+                    <ul>
+                        <li class="text-justify"><?php echo $ul_1_li_8; ?></li>
+                        <li class="text-justify"><?php echo $ul_1_li_9; ?></li>
+                        <li class="text-justify"><?php echo $ul_1_li_10; ?></li>
+                        <li class="text-justify"><?php echo $ul_1_li_11; ?></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 column">
+                    <br><br>
+                    <img alt="" width="230px" class="img-rounded" src="<?php echo base_url('assests/images/info-portal/symptoms_2.jpg'); ?>" />
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row clearfix">
+        <div class="col-md-10 column">
+            <h4>
+                <?php echo $title_3; ?>
+            </h4>
+        </div>
+    </div>
+    <div class="row clearfix">
+        <div class="col-md-10 column col-md-offset-1">
+            <p class="text-justify">
+                <?php echo $text_2; ?>
+            </p>
+            <div class="row clearfix">
+                <div class="col-md-5 column  col-md-offset-1">
+                    <ul>
+                        <li class="text-justify"><?php echo $ul_2_li_1; ?></li>
+                        <li class="text-justify"><?php echo $ul_2_li_2; ?></li>
+                        <li class="text-justify"><?php echo $ul_2_li_3; ?></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 column">
+                    <img alt="" width="230px" class="img-rounded" src="<?php echo base_url('assests/images/info-portal/symptoms_3.jpg'); ?>" />
+                </div>
+            </div>
+            <div class="row clearfix">
+                <div class="col-md-5 column  col-md-offset-1">
+                    <br><br>
+                    <ul>
+                        <li class="text-justify"><?php echo $ul_2_li_4; ?></li>
+                        <li class="text-justify"><?php echo $ul_2_li_5; ?></li>
+                        <li class="text-justify"><?php echo $ul_2_li_6; ?></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 column">
+                    <img alt="" width="230px" class="img-rounded" src="<?php echo base_url('assests/images/info-portal/symptoms_4.jpg'); ?>" />
+                </div>
+            </div>
+        </div>
+        <div class="row clearfix">
+            <div class="col-md-10 column  col-md-offset-1">
+                <br>
+                <p class="text-justify">
+                    <?php echo $text_3 ?>
+                </p>
+            </div>
+
+        </div>
+    </div>
+    <div class="row clearfix">
+        <div class="col-md-10 column">
+            <h4>
+                <?php echo $title_4; ?>
+            </h4>
+        </div>
+    </div>
+    <div class="row clearfix">
+        <div class="col-md-10 column col-md-offset-1">
+            <p class="text-justify">
+                <?php echo $text_4; ?>
+            </p>
+            <div class="row clearfix">
+                <div class="col-md-5 column  col-md-offset-1">
+                    <ul>
+                        <li class="text-justify"><?php echo $ul_3_li_1; ?></li>
+                        <li class="text-justify"><?php echo $ul_3_li_2; ?></li>
+                        <li class="text-justify"><?php echo $ul_3_li_3; ?></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 column">
+                    <img alt="" width="230px" class="img-rounded" src="<?php echo base_url('assests/images/info-portal/symptoms_5.jpg'); ?>" />
+                </div>
+            </div>
+            <div class="row clearfix">
+                <div class="col-md-5 column  col-md-offset-1">
+                    <br><br><br><br>
+                    <ul>
+                        <li class="text-justify"><?php echo $ul_3_li_4; ?></li>
+                        <li class="text-justify"><?php echo $ul_3_li_5; ?></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 column">
+                    <br><br>
+                    <img alt="" width="230px" class="img-rounded" src="<?php echo base_url('assests/images/info-portal/symptoms_6.jpg'); ?>" />
+                </div>
+            </div>
+            <div class="row clearfix">
+                <div class="col-md-5 column  col-md-offset-1">
+                    <br><br><br><br>
+                    <ul>
+                        <li class="text-justify"><?php echo $ul_3_li_6; ?></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 column">
+                    <br><br>
+                    <img alt="" width="230px" class="img-rounded" src="<?php echo base_url('assests/images/info-portal/symptoms_7.jpg'); ?>" />
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row clearfix">
+        <div class="col-md-10 column">
+            <h4>
+                <?php echo $title_5; ?>
+            </h4>
+        </div>
+    </div>
+    <div class="row clearfix">
+        <div class="col-md-10 column col-md-offset-1">
+            <p class="text-justify">
+                <?php echo $text_5; ?>
+            </p>
+            <div class="row clearfix">
+                <div class="col-md-5 column  col-md-offset-1">
+                    <br>
+                    <ul>
+                        <li class="text-justify"><?php echo $ul_4_li_1; ?></li>
+                        <br>
+                        <li class="text-justify"><?php echo $ul_4_li_2; ?></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 column">
+                    <img alt="" width="230px" class="img-rounded" src="<?php echo base_url('assests/images/info-portal/symptoms_8.jpg'); ?>" />
+                </div>
+            </div>
+            <div class="row clearfix">
+                <div class="col-md-5 column  col-md-offset-1">
+                    <br>
+                    <ul>
+                        <li class="text-justify"><?php echo $ul_4_li_3; ?></li>
+                        <br>
+                        <li class="text-justify"><?php echo $ul_4_li_4; ?></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 column">
+                    <br><br>
+                    <img alt="" width="230px" class="img-rounded" src="<?php echo base_url('assests/images/info-portal/symptoms_9.jpg'); ?>" />
+                </div>
+            </div>
+            <div class="row clearfix">
+                <div class="col-md-5 column  col-md-offset-1">
+                    <br><br><br><br>
+                    <ul>
+                        <li class="text-justify"><?php echo $ul_4_li_5; ?></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 column">
+                    <br><br>
+                    <img alt="" width="230px" class="img-rounded" src="<?php echo base_url('assests/images/info-portal/symptoms_10.jpg'); ?>" />
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row clearfix">
+        <div class="col-md-10 column">
+            <h4>
+                <?php echo $title_6; ?>
+            </h4>
+        </div>
+    </div>
+    <div class="row clearfix">
+        <div class="col-md-10 column col-md-offset-1">
+            <p class="text-justify">
+                <?php echo $text_6; ?>
+            </p>
+            <div class="row clearfix">
+                <div class="col-md-5 column  col-md-offset-1">
+                    <br>
+                    <ul>
+                        <li class="text-justify"><?php echo $ul_5_li_1; ?></li>
+                        <br>
+                        <li class="text-justify"><?php echo $ul_5_li_2; ?></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 column">
+                    <img alt="" width="230px" class="img-rounded" src="<?php echo base_url('assests/images/info-portal/symptoms_11.jpg'); ?>" />
+                </div>
+            </div>
+            <div class="row clearfix">
+                <div class="col-md-5 column  col-md-offset-1">
+                    <br><br>
+                    <ul>
+                        <li class="text-justify"><?php echo $ul_5_li_3; ?></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 column">
+                    <br><br>
+                    <img alt="" width="230px" class="img-rounded" src="<?php echo base_url('assests/images/info-portal/symptoms_12.jpg'); ?>" />
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
     <?php $this->load->view('partial/page_footer'); ?>
 </div>
 <?php
