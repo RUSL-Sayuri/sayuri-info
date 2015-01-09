6aa38e0927d446e3b94b46d3c83e9f56b5cf60ed
haritha91 <harithalht@gmail.com>
2015-01-10 00:45:07 +0530

    Added the Asperger syndrome page

--- application/language/english/treatments_lang.php
+++ application/language/english/treatments_lang.php
@@ -1,6 +1,64 @@
 <?php
-$lang['treatments_text_1']='The treatments are...';
+$lang['treatments_title_1']='Treatments';
+$lang['treatments_title_2']='Behavior and Development Programs';
+$lang['treatments_title_3']='Education and Learning Programs';
+$lang['treatments_title_4']='Medications';
+$lang['treatments_title_5']='Other Treatments and Therapies';
+$lang['treatments_title_6']='Speech and language therapy';
+$lang['treatments_title_7']='Art and Music therapy';
+$lang['treatments_title_8']='Occupational therapy';
+$lang['treatments_title_9']='Physical therapy';
+$lang['treatments_title_10']='Treatments to be done at home';
+
+$lang['treatments_text_1']='There is currently no “cure” for autism spectrum disorder (ASD). However, a range of specialist education and behavioural programmes (often referred to as interventions) can be effective in improving the skills of children with ASD.';
+$lang['treatments_text_2']='Some researchers group treatments for ASD into different categories (see figure below). Each category focuses on a type of treatment or on a specific set of symptoms and behaviors that it tries to help.';
+$lang['treatments_text_3']='Because every child with an ASD may show different types of symptoms, a family may need to choose from the available treatments,therapies, and programs based on the child’s needs. The treatment plan for your child may have some of each of these treatment categories.';
+$lang['treatments_text_4']='These programs address social skills, attention,sleep, play, anxiety, parent interaction,challenging behaviors, and other activities. “Early intensive behavioral intervention”programs use specially trained clinicians who work with parents and young children for up to 25 hours every week. Early intensive behavioral intervention programs can last as long as 12 weeks to 3 years.';
+$lang['treatments_text_5']='These programs are offered in schools or other learning centers. They focus on learning and reasoning skills and “whole life” approaches. In Sri Lanka there are few schools which provides this type of education and most of them are limited to main cities such as colombo and kandy.';
+$lang['treatments_text_6']='Medications are provided by the consultant doctors who diagnose the children with autism and medications are provided for various reasons such as to avoid digestion problems, to reduce hyperactive behaviors, etc. to obtain this type of treatment the children should be consulted by a specialist consultant.';
+$lang['treatments_text_7']='There are lot more treatments and therapies other than the ones mentioned above which can be used effectively to address the behaviors and symptoms of autistic children. most of which can be accessed easily than other treatment methods available.some of them are:';
+$lang['treatments_text_8']='Since people with ASDs have deficits in social communication, speech therapy is an important treatment option which allows the child to better express his/ her needs or wants.Some individuals with ASD are nonverbal and unable to develop verbal communication skills, and the use of gestures, sign language, and picture communication programs are often useful tools to improve their abilities to communicate.';
+$lang['treatments_text_9']='Art and music are particularly useful in sensory integration, providing tactile, visual and auditory stimulation. Music therapy is good for speech development and language comprehension. Songs can be used to teach language and increase the ability to put words together. Art therapy provides a nonverbal, symbolic way means of self-expression, and can develop fine motor skills.';
+$lang['treatments_text_10']='Occupational therapy is often used as a treatment for the sensory integration issues associated with ASDs. It is also used to help teach life skills that involve fine-motor movements, such as dressing, using utensils, cutting with scissors, and writing. OT works to improve the individual’s quality of life and ability to participate fully in daily activities. Each occupational therapy program is based on individual evaluations and goals.';
+$lang['treatments_text_11']='Physical therapy is used to improve gross motor skills and handle sensory integration issues, particularly those involving the individual’s ability to feel and be aware of his body in space. Similar to OT, physical therapy is used to improve the individual’s ability to participate in everyday activities. PT works to teach and improve skills such as walking, sitting, coordination, and balance. Physical therapy is most effective when integrated in an early intervention program.';
+$lang['treatments_text_12']='Home is the place where all children spend a lot of time and the place where they learn things first. mothers can make a great deal of change in the childrens lives with their effective contribution. When your child is recognised with autism, carefully go through his/her behaviours, analyze them and follow the treatments which suits your child out of the following.(these are carefully designed set of treatments by the autism center in lady ridgeway hospital colombo)';
         
-        
+$lang['treatments_ul_1_li_1']='Acupuncture';        
+$lang['treatments_ul_1_li_2']='Vitamins and mineral supplements';        
+$lang['treatments_ul_1_li_3']='Massage therapy';        
+$lang['treatments_ul_1_li_4']='The Picture Exchange Communication System';        
+$lang['treatments_ul_1_li_5']='Responsive Prelinguistic Milieu Teaching';        
+$lang['treatments_ul_1_li_6']='Sensory integration therapy';
+
+$lang['treatments_ol_1_li_1']='Bring an object(a toy) to your eye level and make noise of it or sway it.<b>Repeat this until the child looks at your face as a response.</b> ';
+$lang['treatments_ol_1_li_2']='Practise the above activity with different toys and objects.ex: When the child looks at the face blowing soap bubbles, bringing colourful objects.';
+$lang['treatments_ol_1_li_3']='Before you give something to your child bring it to your eye level and ask whether the child wants it. Give the object to the child soon as he/she asks for it looking at your face.ex: When giving biscuits bring it to <b>your eye level</b> and ask “Do you want biscuit?” Give the biscuit only when they respond to it looking at your face.';
+$lang['treatments_ol_1_li_4']='Blow soap balls.child will enjoy it following behind the soap balls.avoid blowing soap balls till the child looks at your face and ask for more.';
+$lang['treatments_ol_1_li_5']='Cover your face with a cloth and go in front of the child, remove the cloth suddenly and surprise the child.take the attention of the child.practise this to your child. Repeat again and again.';
+$lang['treatments_ol_1_li_6']='Cover your face with a mask and take the attention of the child.when the child looks at your face remove the mask.repeat it several times.';
+$lang['treatments_ol_1_li_7']='Paste a dot (or something shining) in your forehead and make the child look at it.';
+$lang['treatments_ol_1_li_8']='Keep the favourite toys of the child in a place where it is difficult to take but can easily be seen (ex: top of a height table) then make the child look at it.';
+$lang['treatments_ol_1_li_9']='Paste two pictures of the food that kid is fond of on your chins. Then ask the child about his choice while keeping his/her attention on you.';
+$lang['treatments_ol_1_li_10']='Train your child to point the things that he/she wants.';
+$lang['treatments_ol_1_li_11']='When playing with the child bend down and have your eye level with that of your child and maintain face to face connection.';
+$lang['treatments_ol_1_li_12']='Always make the child feel that your are around him/her by talking and touching often.';
+$lang['treatments_ol_1_li_13']='When playing practice your children to wait till their turn comes. do not allow him/ her to be in their own world.always involve the child with some activities.';
+$lang['treatments_ol_1_li_14']='Place a moving toy in front of your face. When the child make eye contact with you give the toy to child.';
+$lang['treatments_ol_1_li_15']='Blow a balloon and leave it when the child brings attention on you.';
+$lang['treatments_ol_1_li_16']='Make small pieces of the most favourite food item of your child(ex: biscuit). Give only one piece at a time and give another piece only when the child looks at your face again.do the same for favourite drink items too.';
+$lang['treatments_ol_1_li_17']='Share the feelings with gestures ( Smiling, Clapping etc..)';
+$lang['treatments_ol_1_li_18']='Imitate the child’s speech & the activities and see the reaction of him/her.';
+$lang['treatments_ol_1_li_19']='When engaging in activities with the child raise or lower your voice as necessary to take  the maximum attention of the child. (ex:if attention is low raise your voice and if the child seems to be in panic lower your voice)';
+$lang['treatments_ol_1_li_20']='Ask the favour of your child for the activities ( What do you like to eat? etc… )';
+$lang['treatments_ol_1_li_21']='Make sure to keep your eye level with that of your child and to be very closer to your child when engaging in activities.';
+$lang['treatments_ol_1_li_22']='Increase the activities and conversations done with the child gradually.<b> Use simple words and small sentences.</b>';
+$lang['treatments_ol_1_li_23']='When the child is playing with a car you too bring a similar car and play, while playing exchange the car.use cars of different sizes,shapes and colour too in exchanging.';
+$lang['treatments_ol_1_li_24']='Lay with your child while exchanging the toys.(ex: Give your car to child and take childs car to you.)';
+$lang['treatments_ol_1_li_25']='If two people engage in playing with the child, one person should push the car from one side and the other one should assist the child pushing back the car.';
+$lang['treatments_ol_1_li_26']='Arrange a game or an activity where two people can play in turns,one by one.and practise the child to wait till his/her turn comes.';
+$lang['treatments_ol_1_li_27']='Use toys to imitate day today activities like cooking.';
+$lang['treatments_ol_1_li_28']='Let the child sit in front of you.blow soap bubbles and let the child too to blow soap bubbles.prick the bubbles with your finger and while it explodes make “pop” sound. Repeat the action again and again. If child doesn’t imitate you, take attention of your child towards your lips and encourage him/ her to make “pop” sound.';
+
+
         
         
--- application/views/treatments.php
+++ application/views/treatments.php
@@ -12,28 +12,259 @@ $this->load->view('partial/header');
         
         ?>
         </div>
-        <div class="row clearfix">
+         <div class="row clearfix">
 		<div class="col-md-12 column">
-			<p>
-			<?php
-                        echo $text_1;
-                        ?>	
-                        </p>
+			<h3>
+				 <?php echo $title_1; ?>
+			</h3>
+
+		</div>
+	</div>
+    	<div class="row clearfix">
+		<div class="col-md-9 column col-md-offset-1">
+			
+			<p align="justify">
+				<?php echo $text_1; ?>
+			</p>
+			<p align="justify">
+				<?php echo $text_2; ?>
+                        </p><center><img alt="Treatments_1" width="500px"  class="img-rounded" src="<?php echo base_url('assests/images/info-portal/treatments_1.png'); ?>" /> </center>
+			<p align="justify">
+				<?php echo $text_3; ?>
+			</p>
 		</div>
 	</div>
 	<div class="row clearfix">
-		<div class="col-md-6 column">
+		<div class="col-md-3 column col-md-offset-2">
+                    <img alt="Treatments_2" width="200px" height="200px" align="middle" class="img-rounded" src="<?php echo base_url('assests/images/info-portal/treatments_2.png'); ?>" />
+		</div>
+		<div class="col-md-5 column">
 			<h3>
-				h3. Lorem ipsum dolor sit amet.
+				<?php echo $title_2; ?>
 			</h3>
-			<p>
-				Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
+			<p align="justify">
+				<?php echo $text_4; ?>
 			</p>
 		</div>
-		<div class="col-md-6 column">
-			<img alt="140x140" src="http://lorempixel.com/140/140/" />
+	</div>
+	<div class="row clearfix">
+		<div class="col-md-3 column col-md-offset-2">
+			<img alt="Treatments_3" width="200px" height="200px" align="middle" class="img-rounded" src="<?php echo base_url('assests/images/info-portal/treatments_3.png'); ?>" />
+		</div>
+		<div class="col-md-5 column">
+			<h3>
+				<?php echo $title_3; ?>
+			</h3>
+			<p align="justify">
+				<?php echo $text_5; ?>
+			</p>
 		</div>
 	</div>
+	<div class="row clearfix">
+		<div class="col-md-3 column col-md-offset-2">
+			<img alt="Treatments_4" width="200px" height="200px" align="middle" class="img-rounded" src="<?php echo base_url('assests/images/info-portal/treatments_4.png'); ?>" />
+		</div>
+		<div class="col-md-5 column">
+			<h3>
+				<?php echo $title_4; ?>
+			</h3>
+			<p align="justify">
+				<?php echo $text_6; ?>
+			</p>
+		</div>
+	</div>
+	<div class="row clearfix">
+		<div class="col-md-3 column col-md-offset-2">
+			<img alt="Treatments_5" width="200px" height="200px" align="middle" class="img-rounded" src="<?php echo base_url('assests/images/info-portal/treatments_5.png'); ?>" />
+		</div>
+		<div class="col-md-5 column">
+			<h3>
+				<?php echo $title_5; ?>
+			</h3>
+			<p align="justify">
+				<?php echo $text_7; ?>
+			</p>
+		</div>
+	</div>
+        <br>
+        
+	<div class="row clearfix">
+		<div class="col-md-5 column col-md-offset-2">
+			<h3>
+				<?php echo $title_6; ?>
+			</h3>
+			<p align="justify">
+				<?php echo $text_8; ?>
+			</p>
+		</div>
+		<div class="col-md-3 column">
+			<img alt="Treatments_6" width="300px" height="200px" align="middle" class="img-rounded" src="<?php echo base_url('assests/images/info-portal/treatments_6.jpg'); ?>" />
+		</div>
+	</div>
+        <br>
+	<div class="row clearfix">
+		<div class="col-md-5 column col-md-offset-2">
+			<h3>
+				<?php echo $title_7; ?>
+			</h3>
+			<p align="justify">
+				<?php echo $text_9; ?>
+			</p>
+		</div>
+		<div class="col-md-3 column">
+			<img alt="Treatments_7" width="300px" height="200px" align="middle" class="img-rounded" src="<?php echo base_url('assests/images/info-portal/treatments_7.jpg'); ?>" />
+		</div>
+	</div>
+        <br>
+	
+        <div class="row clearfix">
+		<div class="col-md-5 column col-md-offset-2">
+			<h3>
+				<?php echo $title_8; ?>
+			</h3>
+			<p align="justify">
+				<?php echo $text_10; ?>
+			</p>
+		</div>
+		<div class="col-md-3 column">
+			<img alt="Treatments_8" width="300px" height="200px" align="middle" class="img-rounded" src="<?php echo base_url('assests/images/info-portal/treatments_8.jpg'); ?>" />
+		</div>
+	</div>
+        <br>
+	<div class="row clearfix">
+		<div class="col-md-5 column col-md-offset-2">
+			<h3>
+				<?php echo $title_9; ?>
+			</h3>
+			<p align="justify">
+				<?php echo $text_11; ?>
+			</p>
+			<ul>
+				<li>
+					<?php echo $ul_1_li_1; ?>
+				</li>
+				<li>
+					<?php echo $ul_1_li_2; ?>
+				</li>
+				<li>
+					<?php echo $ul_1_li_3; ?>
+				</li>
+				<li>
+					<?php echo $ul_1_li_4; ?>
+				</li>
+				<li>
+					<?php echo $ul_1_li_5; ?>
+				</li>
+				<li>
+					<?php echo $ul_1_li_6; ?>
+				</li>
+				
+			</ul>
+		</div>
+            <div class="col-md-3 column">
+			<img alt="Treatments_9" width="300px" height="200px" align="middle" class="img-rounded" src="<?php echo base_url('assests/images/info-portal/treatments_9.jpg'); ?>" />
+		</div>
+		
+	</div>
+	<div class="row clearfix">
+		<div class="col-md-9 column col-md-offset-1">
+			<h3>
+				<?php echo $title_10; ?>
+			</h3>
+			<p align="justify">
+				<?php echo $text_12; ?>
+			</p>
+			<ol>
+				<li class="text-justify">
+					<?php echo $ol_1_li_1; ?>
+				</li>
+				<li class="text-justify">
+					<?php echo $ol_1_li_2; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_3; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_4; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_5; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_6; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_7; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_8; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_9; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_10; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_11; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_12; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_13; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_14; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_15; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_16; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_17; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_18; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_19; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_20; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_21; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_22; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_23; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_24; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_25; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_26; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_27; ?>
+				</li>
+                                <li class="text-justify">
+					<?php echo $ol_1_li_28; ?>
+				</li>
+                                
+			</ol>
+		</div>
+	</div>
+
+        
         
     </div>
     
@@ -42,5 +273,7 @@ $this->load->view('partial/header');
 <?php
 $this->load->view('partial/footer');
 ?>
+
+
 
 
