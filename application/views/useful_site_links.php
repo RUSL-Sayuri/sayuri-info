82a547642ae7afb3a786199fbd4220704f5190ab
manojvidanage <manojvidanage@gmail.com>
2015-01-14 01:53:27 +0530

    Useful_site_links page completed .  site pictures are directed to its' web sites. And mouse pointer change on the picture.

--- application/views/useful_site_links.php
+++ application/views/useful_site_links.php
@@ -18,7 +18,7 @@ $this->load->view('partial/header');
                     <p class="text-justify"><?php echo $text_1; ?></p>
                 </div>
                 <div class="col-md-3 column">
-                    <img class="img-responsive img-rounded" alt="" src="<?php echo base_url('assests/images/info-portal/useful_site_links_1.png') ?>" />
+                    <a style="cursor: pointer; " href='http://www.autismspeaks.org' target='_blank'><img class="img-responsive img-rounded" alt="" src="<?php echo base_url('assests/images/info-portal/useful_site_links_1.png') ?>" /></a>
                 </div>
             </div>
         </div>
@@ -31,7 +31,7 @@ $this->load->view('partial/header');
                     <p class="text-justify"><?php echo $text_2; ?></p>
                 </div>
                 <div class="col-md-6 column">
-                    <img class="img-responsive img-rounded" alt="" src="<?php echo base_url('assests/images/info-portal/useful_site_links_2.png') ?>" />
+                    <a style="cursor: pointer; " href='http://www.autism-society.org' target='_blank'><img class="img-responsive img-rounded" alt="" src="<?php echo base_url('assests/images/info-portal/useful_site_links_2.png') ?>" /></a>
                 </div>
             </div>
         </div>
@@ -44,7 +44,7 @@ $this->load->view('partial/header');
                     <p class="text-justify"><?php echo $text_3; ?></p>
                 </div>
                 <div class="col-md-6 column">
-                    <img class="img-responsive img-rounded" alt="" src="<?php echo base_url('assests/images/info-portal/useful_site_links_3.png') ?>" />
+                    <a style="cursor: pointer; " href='http://www.disabilityscoop.com' target='_blank'><img class="img-responsive img-rounded" alt="" src="<?php echo base_url('assests/images/info-portal/useful_site_links_3.png') ?>" /></a>
                 </div>
             </div>
         </div>
@@ -57,7 +57,7 @@ $this->load->view('partial/header');
                     <p class="text-justify"><?php echo $text_4; ?></p>
                 </div>
                 <div class="col-md-3 column">
-                    <img class="img-responsive img-rounded" alt="" src="<?php echo base_url('assests/images/info-portal/useful_site_links_4.png') ?>" />
+                    <a style="cursor: pointer; " href='http://www.autismnj.org' target='_blank'><img class="img-responsive img-rounded" alt="" src="<?php echo base_url('assests/images/info-portal/useful_site_links_4.png') ?>" /></a>
                 </div>
             </div>
         </div>
@@ -70,7 +70,7 @@ $this->load->view('partial/header');
                     <p class="text-justify"><?php echo $text_5; ?></p>
                 </div>
                 <div class="col-md-3 column">
-                    <img class="img-responsive img-rounded" alt="" src="<?php echo base_url('assests/images/info-portal/useful_site_links_5.png') ?>" />
+                    <a style="cursor: pointer; " href='http://www.autism.com' target='_blank'><img class="img-responsive img-rounded" alt="" src="<?php echo base_url('assests/images/info-portal/useful_site_links_5.png') ?>" /></a>
                 </div>
             </div>
         </div>
@@ -84,7 +84,7 @@ $this->load->view('partial/header');
                 </div>
                 <div class="col-md-3 column">
                     <br>
-                    <img class="img-responsive img-rounded" alt="" src="<?php echo base_url('assests/images/info-portal/useful_site_links_6.png') ?>" />
+                    <a style="cursor: pointer; " href='http://www.autismweb.com' target='_blank'><img class="img-responsive img-rounded" alt="" src="<?php echo base_url('assests/images/info-portal/useful_site_links_6.png') ?>" /></a>
                 </div>
             </div>
         </div>
@@ -98,7 +98,7 @@ $this->load->view('partial/header');
                 </div>
                 <div class="col-md-3 column">
                     <br>
-                    <img class="img-responsive img-rounded" alt="" src="<?php echo base_url('assests/images/info-portal/useful_site_links_7.png') ?>" />
+                    <a style="cursor: pointer; " href='http://www.ahany.org' target='_blank'><img class="img-responsive img-rounded" alt="" src="<?php echo base_url('assests/images/info-portal/useful_site_links_7.png') ?>" /></a>
                 </div>
             </div>
         </div>
@@ -111,7 +111,7 @@ $this->load->view('partial/header');
                     <p class="text-justify"><?php echo $text_8; ?></p>
                 </div>
                 <div class="col-md-6 column">
-                    <img class="img-responsive img-rounded" alt="" src="<?php echo base_url('assests/images/info-portal/useful_site_links_8.png') ?>" />
+                    <a style="cursor: pointer; " href='http://www.autismhwy.com' target='_blank'><img class="img-responsive img-rounded" alt="" src="<?php echo base_url('assests/images/info-portal/useful_site_links_8.png') ?>" /></a>
                 </div>
             </div>
         </div>
@@ -124,7 +124,7 @@ $this->load->view('partial/header');
                     <p class="text-justify"><?php echo $text_9; ?></p>
                 </div>
                 <div class="col-md-6 column">
-                    <img class="img-responsive img-rounded" alt="" src="<?php echo base_url('assests/images/info-portal/useful_site_links_9.png') ?>" />
+                    <a style="cursor: pointer; " href='http://www.autismbeacon.com' target='_blank'><img class="img-responsive img-rounded" alt="" src="<?php echo base_url('assests/images/info-portal/useful_site_links_9.png') ?>" /></a>
                 </div>
             </div>
         </div>
@@ -137,7 +137,7 @@ $this->load->view('partial/header');
                     <p class="text-justify"><?php echo $text_10; ?></p>
                 </div>
                 <div class="col-md-3 column">
-                    <img class="img-responsive img-rounded" alt="" src="<?php echo base_url('assests/images/info-portal/useful_site_links_10.png') ?>" />
+                    <a style="cursor: pointer; " href='http://www.autischoolsrilanka.com' target='_blank'><img class="img-responsive img-rounded" alt="" src="<?php echo base_url('assests/images/info-portal/useful_site_links_10.png') ?>" /></a>
                 </div>
             </div>
         </div>
@@ -150,7 +150,7 @@ $this->load->view('partial/header');
                     <p class="text-justify"><?php echo $text_11; ?></p>
                 </div>
                 <div class="col-md-6 column">
-                    <img class="img-responsive img-rounded" alt="" src="<?php echo base_url('assests/images/info-portal/useful_site_links_11.png') ?>" />
+                    <a style="cursor: pointer; " href='http://www.saaac.org' target='_blank'><img class="img-responsive img-rounded" alt="" src="<?php echo base_url('assests/images/info-portal/useful_site_links_11.png') ?>" /></a>
                 </div>
             </div>
         </div>
