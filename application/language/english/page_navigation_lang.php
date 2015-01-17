dc76b3f6b3d2cafd4101a3b6fba9b366b2d65e20
haritha91 <harithalht@gmail.com>
2015-01-18 00:24:43 +0530

    Added the navBar lang file

---
+++ application/language/english/page_navigation_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['navbar_home']="Home";
+$lang['navbar_about_autism']="About Autism";
+$lang['navbar_what_is_autism']="What is Autism";
+$lang['navbar_diagnosis']="Diagnosis";
+$lang['navbar_symptoms']="Symptoms";
+$lang['navbar_treatments']="Treatments";
+$lang['navbar_prevalence']="Prevalence";
+$lang['navbar_asperger_syndrome']="Asperger Syndrome";
+$lang['navbar_resources']="Resources";
+$lang['navbar_useful_links']="Useful Site Links";
+$lang['navbar_learning_kits']="Learning Kits";
+$lang['navbar_article_archive']="Article Archive";
+$lang['navbar_sucess_stories']="Sucess Stories";
+$lang['navbar_centers_in_srilanka']="Autism Centers in Sri Lanka";
+$lang['navbar_app_center']="App Center";
+$lang['navbar_join_us']="Join Us";
+$lang['navbar_about_us']="About Us";
+$lang['navbar_search']="Search";
+
---
+++ application/language/sinhala/page_navigation_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['navbar_home']="මුල් පිටුව";
+$lang['navbar_about_autism']="ඔටිසම්";
+$lang['navbar_what_is_autism']="ඔටිසම් යනු";
+$lang['navbar_diagnosis']="රෝග විනිශ්චය";
+$lang['navbar_symptoms']="රෝග ලක්ෂණ";
+$lang['navbar_treatments']="ප්‍රතිකාර ක්‍රම";
+$lang['navbar_prevalence']="ව්‍යාප්තිය";
+$lang['navbar_asperger_syndrome']="ඇස්පර්ජර් තත්ත්වය";
+$lang['navbar_resources']="සම්පත්";
+$lang['navbar_useful_links']="වැදගත් වෙබ් අඩවි";
+$lang['navbar_learning_kits']="ඉගැනුම් මෙවලම්";
+$lang['navbar_article_archive']="ලිපි ගොනුව";
+$lang['navbar_sucess_stories']="ජය ගත්තෝ ";
+$lang['navbar_centers_in_srilanka']="ශ්‍රී ලංකාවේ විශේෂ පාසල් ";
+$lang['navbar_app_center']="යෙදවුම් එකතුව";
+$lang['navbar_join_us']="එකතු වන්න";
+$lang['navbar_about_us']="සයුරි අපි";
+$lang['navbar_search']="සොයන්න";
+
