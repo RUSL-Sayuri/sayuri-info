f733a923b7b59d531f68151a368c0fe4ebc31fe7
manojvidanage <manojvidanage@gmail.com>
2015-01-18 01:46:46 +0530

    about_us singhala language file attached

--- application/language/sinhala/about_us_lang.php
+++ application/language/sinhala/about_us_lang.php
@@ -6,20 +6,20 @@
  * and open the template in the editor.
  */
 
-$lang['about_us_about_the_team_title']="අප කණ්ඩායම පිලිබඳ";
-$lang['about_us_about_the_team_text']="A set of passionate undergraduates from Rajarata University of Sri Lanka who are willing to provide a unique solution for autistic children of Sri Lanka.";
+$lang['about_us_about_the_team_title']="සයුරි අපි";
+$lang['about_us_about_the_team_text']="අපි ශ්‍රී ලංකා රජරට විශ්වවිද්‍යාලයේ තොරතුරු හා සන්නිවේදන තාක්ෂනය හදාරන  සිසුන් කණ්ඩායමක්  වෙමු. අවසන් වසර  ව්‍යාපෘතිය සදහා  ඔටිසම් ඇති දරුවන් සදහා මේ ව්‍යාපෘතිය නිර්මිතව ඇත.  ";
 
 $lang['about_us_about_the_project_title']="ව්‍යාපෘතිය පිලිබඳ";
-$lang['about_us_about_the_project_text1']="We as a team studied Autism, a disorder of neural development characterized by impaired social interaction in verbal and nonverbal communication along with restricted, repetitive or stereotyped behavior, in depth and came up with a solution which will bring about a great sense of relief to autistic people and their family members from considering the autistic children as a burden or a disabled person.";
-$lang['about_us_about_the_project_text2']="'Sayuri' comprises of this localized trilingual portal and the forum which brings the general public, medical personals working for autism related treatments, occupational therapists, parents of the children on autism spectrum and the general public into a single arena that make them aware of autism and related matters, along with a behavioral training and management materials as apps and applications that can be used for the treatment process with the use of mobile devices as tablets and smart phones which has enhanced mobility and availability for the autistic children.";
-$lang['about_us_about_the_project_text3']="The centralized portal with all the stake holder participation will enhance the awareness of
-the society about autistic children, their symptoms and the treatment methodologies. The interchange of knowledge and experience regarding autism between parents of autistic children and the therapist through the forum will bring about a promising future for the autistic individuals.";
-$lang['about_us_about_the_project_text4']="The set of applications we provide for the autistic children are targeted to improve social and emotional skills in them. Apps are built as games that attract small kids to involve with them closely and they provide self tailored behavioral training and management therapies since the symptoms of the disorder varies from each other.";
+$lang['about_us_about_the_project_text1']="ඔටිසම් තත්වය  පිලිබදව සමාජය දැනුවත් කිරීම සදහාත්, රෝගයෙන් පීඩා විදින්නන්ගේ සන්නිවේදන, සමාජ සහ අනිකුත් හැකියාවන් දියුණු කිරීමේ  අරමුණින් ‘සයුරි’ ව්‍යාපෘතිය නිර්මාණය කර ඇත.";
+$lang['about_us_about_the_project_text2']="සයුරි ලෙස නම් තබා ඇති අපගේ අන්තර්ජාල අඩවිය සිංහල, දමිළ හා ඉංග්‍රීසි යන භාෂා ත්‍රිත්වයෙන් නිර්මාණය කර  ඇත. එමෙන්ම වෛද්‍ය වෘතිකයන්ට සහ විශේෂඤ පුහුණු කරුවන්ට ඖෂධ පිලිබදව සහ ප්‍රතිකාර ක්‍රම පිලිබදව අදහස් හුවමාරු කිරීමට අදාල පහසුකම් ඇත.  පොදු මහජනතාවට තොරතුරු දැනගැනීමට සහ ගැටළු නිරාකරණය කරගැනීමේ පහසුකම් වලින් සමන්විතවේ.";
+$lang['about_us_about_the_project_text3']="මේ පාර්ශවයන් එකිනෙක සම්බන්ධ කිරීම  මගින් රෝගය පිළිබදව නව දැනුම වර්ධනය කරගැනීමටත්, සමාජය තුල රෝගය පිළිබද දැනුවත්බව දියුණු කරගැනීම සදහා මෙන්ම අත්දැකීම් හුවමාරු කරගැනීමට අවස්තාව සැපයීම අපගේ අරමුණයි.";
+$lang['about_us_about_the_project_text4']="විශේෂයෙන් නිර්මාණය කර ඇති පරිගණක ක්‍රීඩාවන් සදහා භාෂා ත්‍රිත්වයෙන්ම සම්බන්ධ වීමේ හැකියාවක් ඇත. පරිඝනකයෙන් පමණක් නොව දුරකතනය මගින් අදාල ක්‍රීඩා වලට සම්බන්ධ වීමට හැක.
+මෙම පරිඝනක ක්‍රීඩාවන් විවිධ වර්ගීකරණයන් යටතේ ඔටිසම් සහිත දරුවන්ගේ කුසලතා වැඩිදියුණු කරගත හැකි අයුරින් නිර්මාණයකර ඇත. ";
 
-$lang['about_us_vision_title']="Vision";
-$lang['about_us_vision_text']="----text----";
+$lang['about_us_vision_title']="දැක්ම.";
+$lang['about_us_vision_text']="ඔටිසම් රෝගීන් නගාසිටුවීම අදාල ක්‍රියාමාර්ග ගැනීමත්, ඔවුන්ට රැකවරණය සහ ප්‍රතිකාර ලබාගැනීමට   මගපෙන්වීම වේ. ";
 
-$lang['about_us_mission_title']="Mission";
-$lang['about_us_mission_text']="---text---";
+$lang['about_us_mission_title']="පරමාර්ථය.";
+$lang['about_us_mission_text']="වෙබ් අඩවිය සහ පරිඝනක ක්‍රීඩා මගින් ඔටිසම් සහිත පුද්ගලයන්ගේ හැකියාවන් දියුණු කිරීම.";
 
 ?>
